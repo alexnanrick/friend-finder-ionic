8ab3228ac707d7f8b4c73f190fa205f007528d9c
Zontzip <zontzor@gmail.com>
2017-05-03 17:30:39 +0100

    Cleanup

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
-export let baseUrl = 'http://127.0.0.1:8000/api';
+// export let baseUrl = 'http://127.0.0.1:8000/api';
 // export let baseUrl = 'http://192.168.1.101:8000/api';
-// export let baseUrl = 'http://ec2-34-248-62-100.eu-west-1.compute.amazonaws.com:8000/api';
+export let baseUrl = 'http://ec2-34-248-62-100.eu-west-1.compute.amazonaws.com:8000/api';
 
 export let mapUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiem9udHppcCIsImEiOiJjajFtbGJrYjEwMDAxMzNvdGs4OXByM2dhIn0.hzPQNENTIuVgt7fPXsUD5Q';
 // export let mapUrl = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}';
